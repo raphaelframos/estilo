8420cc84f79b1c483f48abbbcabc2be574c0181e
raphael <raphael@raphael-Lenovo-G470.(none)>
2013-04-26 22:05:47 -0300

    Criando layout sem estilo

--- gen/br/com/powell/tutorial/estilo/R.java
+++ gen/br/com/powell/tutorial/estilo/R.java
@@ -24,7 +24,21 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f08000e;
+        public static final int editText1=0x7f080003;
+        public static final int editText2=0x7f080006;
+        public static final int editText3=0x7f080009;
+        public static final int editText4=0x7f08000c;
+        public static final int tableRow1=0x7f080001;
+        public static final int tableRow2=0x7f080004;
+        public static final int tableRow3=0x7f080007;
+        public static final int tableRow4=0x7f08000a;
+        public static final int textView1=0x7f080000;
+        public static final int textView2=0x7f080002;
+        public static final int textView3=0x7f080005;
+        public static final int textView4=0x7f080008;
+        public static final int textView5=0x7f08000b;
+        public static final int textView6=0x7f08000d;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -35,7 +49,12 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int cidade=0x7f050007;
+        public static final int email=0x7f050006;
+        public static final int formulario=0x7f050003;
         public static final int hello_world=0x7f050002;
+        public static final int nome=0x7f050004;
+        public static final int sobrenome=0x7f050005;
     }
     public static final class style {
         /** 
